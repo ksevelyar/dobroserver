50f2ed689db871b94b12edb9bb4e860c095542f3
ksevelyar <ksevelyar@gmail.com>
2014-06-04 10:33:01 +0400

    removed the limit on message length

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,4 @@ class Message
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }
-
-  validates_length_of :content, maximum: 600
 end
